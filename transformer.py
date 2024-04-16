5818b30f04d5808355adc6a78c55f4fb40eb9fc2
Christopher McGale <56483395+chrismcgale@users.noreply.github.com>
2024-04-16 12:24:37 -0400

    Added layernorm and scaled model

--- transformer.py
+++ transformer.py
@@ -4,14 +4,17 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # hyperparameters
-batch_size = 32 # number of independent sequences processed in parallel
-block_size = 8 # max context length for predictions
+batch_size = 64 # number of independent sequences processed in parallel
+block_size = 256 # max context length for predictions
 max_iters = 5000
 eval_iter = 500
-learning_rate = 1e-3
+learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 32
+n_embd = 384
+n_head = 6
+n_layer = 6
+dropout = 0.2
 #----------------
 
 torch.manual_seed(1337)
@@ -68,6 +71,7 @@ class Head(nn.Module):
         self.query = nn.Linear(n_embd, head_size, bias=False)
         self.value = nn.Linear(n_embd, head_size, bias=False)
         self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+        self.dropout = nn.Dropout(dropout)
         
         
     def forward(self, x):
@@ -78,6 +82,8 @@ class Head(nn.Module):
         wei = q @ k.transpose(-2, -1) * C**-0.5 # (B,T,C) @ (B,C,T) --> (B, T, T)
         wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
         wei = F.softmax(wei, dim=-1)
+        
+        wei = self.dropout(wei)
         
         v = self.value(x)
         out = wei @ v # (B, T, T) @ (B, T, C) --> (B, T, C)
@@ -91,11 +97,12 @@ class MultiHeadAttention(nn.Module):
         super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
         self.proj = nn.Linear(n_embd, n_embd)
+        self.dropout = nn.Dropout(dropout)
         
         
     def forward(self, x):
         out = torch.cat([h(x) for h in self.heads], dim=-1)
-        out = self.proj(out)
+        out = self.dropout(self.proj(out))
         return out
     
     
@@ -108,6 +115,7 @@ class FeedFoward(nn.Module):
             nn.Linear(n_embd, 4 * n_embd),
             nn.ReLU(),
             nn.Linear(4 * n_embd, n_embd),
+            nn.Dropout(dropout),
         )
         
     def forward(self, x):
@@ -122,10 +130,12 @@ class Block(nn.Module):
         head_size = n_embd // n_head
         self.sa = MultiHeadAttention(n_head, head_size)
         self.ffwd = FeedFoward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
         
     def forward(self, x):
-        x = x + self.sa(x)
-        x = x + self.ffwd(x)
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
         return x
 
 class BigramLanguageModel(nn.Module):
@@ -133,11 +143,8 @@ class BigramLanguageModel(nn.Module):
         super().__init__()
         self.token_embdding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embdding_table = nn.Embedding(block_size, n_embd)
-        self.blocks = nn.Sequential(
-            Block(n_embd, n_head=4),
-            Block(n_embd, n_head=4),
-            Block(n_embd, n_head=4),
-        )
+        self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embd)
         self.lm_head = nn.Linear(n_embd, vocab_size)
         
     def forward(self, idx, targets=None):
@@ -147,6 +154,7 @@ class BigramLanguageModel(nn.Module):
         pos_emb = self.position_embdding_table(torch.arange(T, device=device)) # (T, C)
         x = tok_emb + pos_emb # (B, T, C)
         x = self.blocks(x)
+        x = self.ln_f(x)
         logits = self.lm_head(x) # (B,T,vocab_size)
         
         if targets == None:
@@ -181,6 +189,24 @@ class BigramLanguageModel(nn.Module):
             idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
             
         return idx
+    
+class BatchNorm1d:
+    def __init__(self, dim, eps=1e-5, momentum=0.1):
+        self.eps = eps
+        self.gamma = torch.ones(dim)
+        self.beta = torch.zeros(dim)
+    
+    def __call__(self, x):
+        xmean = x.mean(1, keepdim=True)
+        xvar = x.var(1, keepdim=True)
+        
+        xhat = (x - xmean) / torch.sqrt(xvar + self.eps) # normalize to unit var
+        self.out = self.gamma * xhat + self.beta
+        return self.out
+        
+    def parameters(self):
+        return [self.gamma, self.beta]
+    
     
 model = BigramLanguageModel()
 m = model.to(device)
