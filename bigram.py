829fb389d05d7225057c523fe2a826840fbb02d6
Christopher McGale <56483395+chrismcgale@users.noreply.github.com>
2024-04-16 11:12:31 -0400

    Scaled dot product attention

--- bigram.py
+++ bigram.py
@@ -6,9 +6,9 @@ from torch.nn import functional as F
 # hyperparameters
 batch_size = 32 # number of independent sequences processed in parallel
 block_size = 8 # max context length for predictions
-max_iters = 3000
-eval_iter = 300
-learning_rate = 1e-2
+max_iters = 5000
+eval_iter = 500
+learning_rate = 1e-3
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
 n_embed = 32
@@ -59,20 +59,46 @@ def estimate_loss():
     model.train()
     return out
 
+class Head(nn.Module):
+    """ one head of self-attention """
+    
+    def __init__(self, head_size):
+        super().__init__()
+        self.key = nn.Linear(n_embed, head_size, bias=False)
+        self.query = nn.Linear(n_embed, head_size, bias=False)
+        self.value = nn.Linear(n_embed, head_size, bias=False)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+        
+        
+    def forward(self, x):
+        B,T,C = x.shape
+        k = self.key(x) # (B,T,C)
+        q = self.query(x) # (B,T,C)
+        # compute attention scores ("affinities")
+        wei = q @ k.transpose(-2, -1) * C**-0.5 # (B,T,C) @ (B,C,T) --> (B, T, T)
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
+        wei = F.softmax(wei, dim=-1)
+        
+        v = self.value(x)
+        out = wei @ v # (B, T, T) @ (B, T, C) --> (B, T, C)
+        return out
+
 class BigramLanguageModel(nn.Module):
     def __init__(self):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size, n_embed)
         self.position_embedding_table = nn.Embedding(block_size, n_embed)
+        self.sa_head = Head(n_embed)
         self.lm_head = nn.Linear(n_embed, vocab_size)
         
     def forward(self, idx, targets=None):
-        
+        B,T = idx.shape
         # idx and targets are both (B,T) tensor of integers
         tok_emb = self.token_embedding_table(idx) # (Batch, Time, Channel)
-        pos_emb = self.position_embedding_table(torch.arrange(T, device=device)) # (T, C)
+        pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T, C)
         x = tok_emb + pos_emb # (B, T, C)
-        logits = self.lm_head(tok_emb) # (B,T,vocab_size)
+        x = self.sa_head(x)
+        logits = self.lm_head(x) # (B,T,vocab_size)
         
         if targets == None:
             loss = None
@@ -89,7 +115,11 @@ class BigramLanguageModel(nn.Module):
     
     def generate(self, idx, max_new_tokens):
         for _ in range(max_new_tokens):
-            logits, loss = self(idx)
+            # crop idx to the last block_size tokens
+            idx_cond = idx[:, -block_size:]
+            
+            # get the predictions
+            logits, loss = self(idx_cond)
             
             logits = logits[:, -1, :] # becomes (B, C)
             
